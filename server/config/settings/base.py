26a3e5fde94a32638d5aeac373492a213ad59f97
Neeschal1 <neeschalpok04@gmail.com>
2026-08-12 12:06:00 +0545

    md: cors trusted origin updation for accepting the deployed server url

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -75,7 +75,12 @@ CORS_ALLOW_METHODS = (
 )
 
 CORS_ALLOW_ALL_ORIGINS = True
-CSRF_TRUSTED_ORIGINS = ["https://dos-nlp-2.onrender.com/", "https://server.dosnpl.com"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://dos-nlp-2.onrender.com",
+    "https://server.dosnpl.com",
+    "https://www.dosnpl.com",
+    "https://dosnpl.com",
+]
 
 
 REST_FRAMEWORK = {
